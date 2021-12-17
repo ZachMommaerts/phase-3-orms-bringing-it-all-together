9f4cef1e1f7bb2aed44186b05b3841c89a4ffb5a
Zach <zmommaerts@gmail.com>
2021-12-16 22:29:15 -0600

    stuck

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    attr_accessor :id, :name, :breed
 
+    def initialize(name, breed)
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+            SQL
+        DB[:conn].execute(sql)
+    end
 end
